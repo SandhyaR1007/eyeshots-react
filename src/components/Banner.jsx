056945d06468df10a2476857b42944f0f47262c0
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-14 02:57:54 +0530

    add: Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,35 @@
+import { BsArrowDownRightCircle } from "react-icons/bs";
+
+import bannerImg from "@/assets/bannerImg.png";
+
+const Banner = () => {
+  return (
+    <main className=" flex justify-between items-center py-4 my-5 font-josephin relative">
+      <section className="max-w-xl mx-auto sm:mx-0  sm:w-2/3 p-3  ">
+        <h1 className="text-8xl sm:text-6xl md:text-7xl font-semibold  py-3">
+          Glasses
+          <br />& Lens
+        </h1>
+        <p className="py-3 text-md  text-gray-600">
+          Buy the best high-quality sunglasses from us.
+          <br />
+          More than 100 types of assortment.
+        </p>
+        <section className="flex items-center">
+          <button className="py-2 px-4 bg-gray-900 text-sm text-gray-100 rounded-lg hover:bg-gray-800">
+            Start Shopping
+          </button>
+          <button className="p-3 flex items-center">
+            <span className="mx-2">Explore More</span>{" "}
+            <BsArrowDownRightCircle className="text-lg" />
+          </button>
+        </section>
+      </section>
+      <section className="hidden sm:block w-1/3">
+        <img src={bannerImg} alt="bannerImg" width={350} />
+      </section>
+    </main>
+  );
+};
+
+export default Banner;
