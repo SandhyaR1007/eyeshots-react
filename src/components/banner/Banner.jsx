75636cf46418ef30bf0825b6188746b39428b7ab
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-14 15:36:18 +0530

    update: width classes

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,8 +5,8 @@ import bannerImg from "@/assets/bannerImg.png";
 const Banner = () => {
   return (
     <main className=" flex justify-between items-center py-4 my-5 font-josephin relative">
-      <section className="max-w-xl mx-auto sm:mx-0  sm:w-2/3 p-3  ">
-        <h1 className="text-8xl sm:text-6xl md:text-7xl font-semibold  py-3 w-full md:w-1/2">
+      <section className="max-w-xl mx-auto sm:mx-0  w-full p-3  lg:w-1/3">
+        <h1 className="text-8xl  md:text-7xl lg:text-8xl font-semibold  py-3 w-full ">
           Glasses & Lens
         </h1>
         <p className="py-3 text-md  text-gray-600">
@@ -22,8 +22,8 @@ const Banner = () => {
           </button>
         </section>
       </section>
-      <section className="hidden sm:block w-1/3">
-        <img src={bannerImg} alt="bannerImg" width={350} />
+      <section className="hidden lg:block w-1/2">
+        <img src={bannerImg} alt="bannerImg" className="w-full h-full" />
       </section>
     </main>
   );
