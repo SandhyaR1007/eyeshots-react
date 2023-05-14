02972235c86dc05d3bb83dd632ed34f50164f0f8
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-14 15:35:23 +0530

    update: horizontal margin for Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Banner, Navbar, Trending } from "../components";
 
 const Home = () => {
   return (
-    <div className="mx-[10%]">
+    <div className="mx-[5%] md:mx-[10%]">
       <Navbar />
       <Banner />
       <Trending />
