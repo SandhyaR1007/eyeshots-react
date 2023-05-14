5aba31284233f0933eab45f54c2924263f65f91b
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-14 14:19:51 +0530

    update: move Banner component to banner folder

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -6,9 +6,8 @@ const Banner = () => {
   return (
     <main className=" flex justify-between items-center py-4 my-5 font-josephin relative">
       <section className="max-w-xl mx-auto sm:mx-0  sm:w-2/3 p-3  ">
-        <h1 className="text-8xl sm:text-6xl md:text-7xl font-semibold  py-3">
-          Glasses
-          <br />& Lens
+        <h1 className="text-8xl sm:text-6xl md:text-7xl font-semibold  py-3 w-full md:w-1/2">
+          Glasses & Lens
         </h1>
         <p className="py-3 text-md  text-gray-600">
           Buy the best high-quality sunglasses from us.
@@ -16,9 +15,7 @@ const Banner = () => {
           More than 100 types of assortment.
         </p>
         <section className="flex items-center">
-          <button className="py-2 px-4 bg-gray-900 text-sm text-gray-100 rounded-lg hover:bg-gray-800">
-            Start Shopping
-          </button>
+          <button className="btn-primary">Start Shopping</button>
           <button className="p-3 flex items-center">
             <span className="mx-2">Explore More</span>{" "}
             <BsArrowDownRightCircle className="text-lg" />
